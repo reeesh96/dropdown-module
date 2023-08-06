1772018887e3b7b85c3408418bb82e1b77620eee
Rishabh <saxena12.purdue@gmail.com>
2023-08-06 11:53:12 -0700

    clean up styles and add toggle and item click

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -2,28 +2,46 @@
 
 import { useState } from "react";
 import { DropdownProps } from "./config";
+import './index.css'
 
 export default function Dropdown(props: DropdownProps) {
     const { title, options } = props
     const [ isCollapsed, setIsCollapsed ] = useState(true)
 
+    function handleItemClick(item: string) {
+        console.log(`You selected: ${item}`)
+    }
+
     return (
-        <div className="flex flex-col items-start text-base w-[19rem]">
+        <div className="dropdown-container">
             <p>{title}</p>
-            <div className="relative w-full flex flex-row" onClick={() => setIsCollapsed(!isCollapsed)}>
-                <div className="w-full h-[2rem] border-solid border-2 border-indigo-600">
+            <div className="relative w-full h-fit">
+                <div
+                    className="dropdown-element"
+                    onClick={() => setIsCollapsed(!isCollapsed)}
+                >
+                    <div className="w-full h-full">
+                    </div>
+                    <div className="px-[1rem]">
+                        {isCollapsed ? 'V' : '^'}
+                    </div>
                 </div>
-                <div className="px-[1rem]">
-                    {isCollapsed ? 'V' : '^'}
-                </div>
-                <div className="absolute mt-[2rem] h-fit w-full">
-                    {
-                        options.map((option, index) => {
-                            return (
-                                <div key={`${option}_${index}`}>{option}</div>
-                            )
-                        })
-                    }
+                <div hidden={isCollapsed} className="dropdown-menu-container">
+                    <div className="flex flex-col items-start">
+                        {
+                            options.map((option, index) => {
+                                return (
+                                    <div
+                                        className="dropdown-menu-item"
+                                        key={`${option}_${index}`}
+                                        onClick={() => handleItemClick(option)}
+                                    >
+                                        {option}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </div>
